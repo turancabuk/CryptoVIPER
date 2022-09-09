0b30abd60185bba7c5514602c9e35034b488adda
turan çabuk <turan6677@gmail.com>
2022-09-09 21:44:06 +0300

    viewDidLoad added. UITableViewController added.

--- CryptoVIPER/View.swift
+++ CryptoVIPER/View.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 // talks to -> Presenter
 // Class , Protocols
@@ -13,10 +14,71 @@ import Foundation
 
 protocol AnyView {
     
-
+    var presenter : AnyPresenter?  { get set }
+    
+    
+    func update(with crytos : [Crypto])
+    func update(with error: String)
 }
 
-class CryptoView : AnyView {
+class CryptoViewController : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    var presenter: AnyPresenter?
+    var cryptos : [Crypto] = []
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let messageLabel : UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = .blue
+        label.textAlignment = .center
+        return label
+    }()
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+        
+        tableView.frame = view.bounds
+        messageLabel.frame = CGRect(x: view.frame.width / 2, y: view.frame.height / 2, width: 200, height: 50)
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func update(with crytos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = crytos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    func update(with error: String) {
+        
+    }
+    
+    
     
     
 }
